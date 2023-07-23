b0134855a507144b5c457a46e45d08ca60458860
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2023-07-23 02:00:47 -0500

    Update nvim-tree navigation

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -4,15 +4,30 @@ local nvimtree = require("nvim-tree")
 
 nvimtree.setup({
     disable_netrw = true,
-    sync_root_with_cwd = true,
     respect_buf_cwd = true,
-    update_focused_file = { update_root = true },
+    update_focused_file = {
+        enable = true,
+        update_root = true
+    },
     view = {
         side = 'right',
     },
-    actions ={
+    actions = {
         open_file = {
             quit_on_open = true,
+
+            resize_window = true,
+            window_picker = {
+                enable = true,
+                exclude = {
+                    filetype = { 'notify', 'plug', 'qf', 'diff', 'fugitive', 'fugitiveblame' },
+                    buftype = { 'nofile', 'terminal', 'help' },
+                },
+            },
+        },
+        change_dir = {
+            enable = true,
+            global = true
         }
     },
 })
