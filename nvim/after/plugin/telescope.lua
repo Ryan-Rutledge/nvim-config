c4beecc12d2544cd3233187436d173159faeb862
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2023-07-24 00:49:19 -0500

    Improve telescope file search keymaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 local action = require('telescope.actions')
+local utils = require('telescope.utils')
 local action_layout = require('telescope.actions.layout')
 
 telescope.setup({
@@ -55,10 +56,11 @@ vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
 
 
 -- files
-vim.keymap.set('n', '<leader>F', builtin.find_files, {})
 vim.keymap.set('n', '<leader>//', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>/*', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>F', builtin.find_files, {})
 vim.keymap.set('n', '<leader>/f', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>/b', function() builtin.live_grep({ grep_open_files = true }) end, {})
+vim.keymap.set('n', '<leader>/*', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>/o', builtin.treesitter, {})
 vim.keymap.set('n', '<leader>/s', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>/S', builtin.lsp_workspace_symbols, {})
@@ -68,8 +70,9 @@ vim.keymap.set('n', '<leader>/d', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>/D', function() builtin.diagnostics({ bufnr = nil }) end, {})
 
 -- vim
-vim.keymap.set('n', '<leader>/?', builtin.search_history, {})
+vim.keymap.set('n', '<leader>`', builtin.buffers, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>/?', builtin.search_history, {})
 vim.keymap.set('n', '<leader>r', builtin.registers, {})
 vim.keymap.set('n', '<leader>/:', builtin.command_history, {})
 vim.keymap.set('n', '<leader>/<F1>', builtin.help_tags, {})
