c2825e8a7515268fcca6b90b501a11800c47659f
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2026-06-08 01:52:19 -0500

    gitsigns next/prev keymaps include staged hunks

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -7,13 +7,13 @@ require('gitsigns').setup({
 
         vim.keymap.set('n', ']c', function()
             if vim.wo.diff then return ']c' end
-            vim.schedule(gs.next_hunk)
+            vim.schedule(function () gs.next_hunk({ target = 'all' }) end)
             return '<Ignore>'
         end, { buffer = bufnr, silent = true, noremap = true, expr=true})
 
         vim.keymap.set('n', '[c', function()
             if vim.wo.diff then return '[c' end
-            vim.schedule(gs.prev_hunk)
+            vim.schedule(function() gs.prev_hunk({ target = 'all' }) end)
             return '<Ignore>'
         end, { buffer = bufnr, silent = true, noremap = true, expr=true})
 
@@ -21,14 +21,12 @@ require('gitsigns').setup({
         vim.keymap.set('n', '<leader>gp', gs.preview_hunk_inline, mopts)
         vim.keymap.set('n', '<leader>gP', gs.preview_hunk, mopts)
         vim.keymap.set('n', '<leader>gr', gs.reset_hunk, mopts)
-        vim.keymap.set('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, mopts)
+        vim.keymap.set('v', '<leader>gr', function() gs.reset_hunk({vim.fn.line('.'), vim.fn.line('v')}) end, mopts)
         vim.keymap.set('n', '<leader>gR', gs.reset_buffer, mopts)
         vim.keymap.set('n', '<leader>gs', gs.stage_hunk, mopts)
-        vim.keymap.set('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, mopts)
+        vim.keymap.set('v', '<leader>gs', function() gs.stage_hunk({vim.fn.line('.'), vim.fn.line('v')}) end, mopts)
         vim.keymap.set('n', '<leader>gS', gs.stage_buffer, mopts)
         vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk, mopts)
         vim.keymap.set('n', '<leader>gw', gs.toggle_word_diff, mopts)
-        vim.keymap.set({'o', 'x'}, 'ic', gs.select_hunk, mopts)
-        vim.keymap.set({'o', 'x'}, 'ac', gs.select_hunk, mopts)
     end
 })
